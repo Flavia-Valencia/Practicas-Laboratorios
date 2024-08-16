8ee24ab32e67acd6703084d3aeee75c3c4f81b63
Flavia-Valencia <yasuriyamileth2704@gmail.com>
2024-08-16 11:54:32 -0600

    Flavia Valencia
    Ejercicio 1

---
+++ ParoImpar.js
@@ -0,0 +1,14 @@
+function numParImpar(numero) {
+    return numero
+}
+
+let numero1= numParImpar(4) 
+if (numero1 % 2==0 ) {
+    console.log(`El numero ${numero1} es: Verdadero `);
+} else {
+    console.log(`El numero ${numero1} es: Falso `);
+}
+
+
+
+
