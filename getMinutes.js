d4ff688b04af99b813ac04d1bdc023710e3e8239
Flavia-Valencia <yasuriyamileth2704@gmail.com>
2024-08-30 10:42:04 -0600

    Flavia Valencia
    Practica-3

---
+++ getMinutes.js
@@ -0,0 +1,8 @@
+// Crear un objeto Date con la fecha y horas actuales
+const fecha_actuales = new Date()
+
+// Obtener los minutos actuales 
+const minutos = fecha_actuales.getMinutes()
+
+console.log(`Minutos actuales: ${minutos}`);
+
---
+++ getSeconds.js
@@ -0,0 +1,7 @@
+// Crear un objeto Date con la fecha y horas actuales 
+const fecha_actuales = new Date()
+
+// Obtener los segundos actuales 
+const segundos = fecha_actuales.getSeconds()
+
+console.log(`Segundos actuales: ${segundos}`);
